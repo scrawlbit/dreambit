888033555d584dd2995223498bc86db40c592697
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2020-03-03 13:12:05 -0300

    edição do nome do game object

---
+++ DreamBit.Extension/Controls/TreeViews/EditableTreeViewItemText.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Scrawlbit.Helpers;
+using Scrawlbit.Presentation.Dependency;
+using Scrawlbit.Presentation.Helpers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DreamBit.Extension.Controls.TreeViews
+{
+    public partial class EditableTreeViewItemText : UserControl
+    {
+        public delegate void TextChangedEventArgs(object sender, (object OldValue, object NewValue) e);
+        private static readonly DependencyProperty<EditableTreeViewItemText, string> TextProperty;
+        public event TextChangedEventArgs Changed;
+
+        static EditableTreeViewItemText()
+        {
+            var dependency = new DependencyRegistry<EditableTreeViewItemText>();
+
+            TextProperty = dependency.Property(e => e.Text);
+        }
+        public EditableTreeViewItemText()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        public string Text
+        {
+            get { return TextProperty.Get(this); }
+            set { TextProperty.Set(this, value); }
+        }
+        private bool EditingMode
+        {
+            get { return EditingText.Visibility == Visibility.Visible; }
+            set
+            {
+                DisplayText.Visibility = value ? Visibility.Collapsed : Visibility.Visible;
+                EditingText.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var treeViewItem = this.ParentsUntil<TreeViewItem>();
+
+            treeViewItem.KeyDown += OnTreeViewItemKeyDown;
+            treeViewItem.Unselected += OnTreeViewItemUnselected;
+
+            Loaded -= OnLoaded;
+        }
+        private void OnTreeViewItemUnselected(object sender, RoutedEventArgs e)
+        {
+            if (EditingMode)
+                CancelEdition();
+        }
+        private void OnTreeViewItemKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Handled) return;
+
+            if (e.Key == Key.F2 && !EditingMode)
+            {
+                StartEditing();
+                e.Handled = true;
+            }
+
+            if (e.Key == Key.Enter && EditingMode)
+            {
+                ApplyChanges();
+                e.Handled = true;
+            }
+
+            if (e.Key == Key.Escape && EditingMode)
+            {
+                CancelEdition();
+                e.Handled = true;
+            }
+        }
+
+        private void StartEditing()
+        {
+            EditingMode = true;
+            EditingText.Text = Text;
+            EditingText.Focus();
+            EditingText.SelectAll();
+        }
+        private void ApplyChanges()
+        {
+            if (!EditingText.Text.HasValue())
+                return;
+
+            var oldValue = Text;
+            var newValue = EditingText.Text.Trim();
+
+            Text = newValue;
+            CancelEdition();
+            Changed?.Invoke(this, (oldValue, newValue));
+        }
+        private void CancelEdition()
+        {
+            EditingMode = false;
+        }
+    }
+}
--- DreamBit.Extension/Windows/SceneHierarchyView.xaml.cs
+++ DreamBit.Extension/Windows/SceneHierarchyView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using DreamBit.Extension.Helpers;
+﻿using DreamBit.Extension.Controls.TreeViews;
+using DreamBit.Extension.Helpers;
 using DreamBit.Extension.ViewModels;
+using Scrawlbit.Presentation.Helpers;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace DreamBit.Extension.Windows
 {
@@ -11,6 +15,36 @@ namespace DreamBit.Extension.Windows
             if (this.IsInDesignMode()) return;
 
             LoadViewModel<SceneHierarchyViewModel>();
+        }
+
+        private void OnTextLoaded(object sender, RoutedEventArgs e)
+        {
+            // TODO
+
+            //var editable = (EditableTreeViewItemText)sender;
+            //var border = editable.ParentsUntil<Border>();
+
+            //border.AddBehavior<TreeViewDraggableBehavior>();
+            //border.AddBehavior(new ExtendedTreeViewDroppableBehavior
+            //{
+            //    DropCommand = ViewModel.MoveGameObjectCommand
+            //});
+
+            //editable.Loaded -= OnTextLoaded;
+        }
+        private void OnTextChanged(object sender, (object OldValue, object NewValue) e)
+        {
+            // TODO
+
+            //var editable = (EditableTreeViewItemText)sender;
+            //var gameObject = (IGameObject)editable.DataContext;
+
+            //gameObject.TrackProperty(
+            //    p => p.Name,
+            //    e.OldValue,
+            //    e.NewValue,
+            //    Resource.GameObjectRenameStringFormat.FormatWith(e.OldValue, e.NewValue)
+            //);
         }
     }
 }
