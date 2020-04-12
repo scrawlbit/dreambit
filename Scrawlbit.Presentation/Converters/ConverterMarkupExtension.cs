36e5c0778c573c33d49e139bcb25583e8cd5ca37
Thiago Romam <thiago.romam@outlook.com>
2020-04-11 23:02:33 -0300

    melhoria de converters

--- Scrawlbit.Presentation/Converters/ConverterMarkupExtension.cs
+++ Scrawlbit.Presentation/Converters/ConverterMarkupExtension.cs
@@ -11,21 +11,9 @@ namespace Scrawlbit.Presentation.Converters
 {
     public abstract class ConverterMarkupExtension : MarkupExtension, IValueConverter, IMultiValueConverter
     {
-        private readonly IDictionary<Type, object> _converters;
-
-        public ConverterMarkupExtension()
-        {
-            _converters = new Dictionary<Type, object>();
-        }
-
         public sealed override object ProvideValue(IServiceProvider serviceProvider)
         {
-            Type type = GetType();
-
-            if (!_converters.ContainsKey(type))
-                _converters[type] = Activator.CreateInstance(type);
-
-            return _converters[type];
+            return this;
         }
 
         public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
