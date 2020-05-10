80ee0923273f59ae21f1c031d59c90d8142de70b
Thiago Romam <thiago.romam@outlook.com>
2020-05-09 22:50:00 -0300

    adição do campo de alvo do componente de camera

--- DreamBit.Extension/Windows/SceneInspect/CameraInspect.xaml.cs
+++ DreamBit.Extension/Windows/SceneInspect/CameraInspect.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using DreamBit.Extension.Controls.Input;
+using DreamBit.Game.Elements;
 using DreamBit.Game.Elements.Components;
 using DreamBit.General.State;
 using Newtonsoft.Json.Linq;
 using Scrawlbit.Helpers;
+using Scrawlbit.Util.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +39,14 @@ namespace DreamBit.Extension.Windows.SceneInspect
                 ViewModel.State.Add(newChange);
                 ViewModel.State.Execute(oldChange);
             }
+        }
+        private void OnTargetChanged(GameObjectSelector sender, ValueChangedEventArgs<Guid> e)
+        {
+            GameObject target = ViewModel.Editor.OpenedScene.Objects.Find(e.NewValue);
+            string description = $"{Camera.GameObject.Name}'s camera target set to \"{target?.Name}\"";
+            IStateCommand command = Camera.State().SetProperty(c => c.Target, e, description);
+
+            ViewModel.State.Add(command);
         }
     }
 }
--- DreamBit.Game/Elements/Components/Camera.cs
+++ DreamBit.Game/Elements/Components/Camera.cs
@@ -1,11 +1,13 @@
 ﻿using DreamBit.Game.Drawing;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace DreamBit.Game.Elements.Components
 {
     public class Camera : GameComponent
     {
         private bool _active;
+        private Guid _target;
 
         public override string Name => "Camera";
         private Vector2 Size => new Vector2(800, 600);
@@ -13,6 +15,11 @@ namespace DreamBit.Game.Elements.Components
         {
             get => _active;
             set => Set(ref _active, value);
+        }
+        public Guid Target
+        {
+            get => _target;
+            set => Set(ref _target, value);
         }
 
         protected internal override void Draw(IContentDrawer drawer)
